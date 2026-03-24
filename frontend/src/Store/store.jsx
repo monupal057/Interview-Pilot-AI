9148e1a88315ebc3a2e2a310d0224c7fddd159cc
MONU PAL <monupal@192.168.1.52>
2026-03-25 00:27:16 +0530

    update

--- frontend/src/Store/store.jsx
+++ frontend/src/Store/store.jsx
@@ -1,10 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
-import  useReducer from './user.Reducer.jsx'
+// import { configureStore } from "@reduxjs/toolkit";
+// import  useReducer from './user.Reducer.jsx'
 
 
-export const store = configureStore({
-    reducer:{
-        userReducer:userReducer
-    }
+// export const store = configureStore({
+//     reducer:{
+//         userReducer:userReducer
+//     }
 
+// })
+import { configureStore } from "@reduxjs/toolkit"
+import userReducer from "./user.Reducer"
+
+const store = configureStore({
+  reducer: {
+    user: userReducer
+  }
 })
+
+export default store
--- frontend/src/Store/user.Reducer.jsx
+++ frontend/src/Store/user.Reducer.jsx
@@ -1,24 +1,47 @@
+// import { createSlice } from "@reduxjs/toolkit"
+
+// const initialState = {
+//     isAuthenticate: false
+
+// }
+
+// const userSlice = createSlice({
+
+//     name:"user",
+//     initialState,
+//     reducers:{
+//         addUser:(state)=> {
+//             state.isAuthenticate = true
+//         }, 
+//            removeUser:(state,action)=> {
+//             state.isAuthenticate = false
+//     }
+// }
+// })
+
+//  export default userSlice.reducer;
+//  export const {addUser, removeUser} = userSlice.actions
+
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    isAuthenticate: false
-
+  isAuthenticated: false
 }
 
 const userSlice = createSlice({
-
-    name:"user",
-    initialState,
-    reducers:{
-        addUser:(state,action)=> {
-            state.isAuthenticate = true
-        }, 
-           removeUser:(state,action)=> {
-            state.isAuthenticate = false
+  name: "user",
+  initialState,
+  reducers: {
+    addUser: (state) => {
+      state.isAuthenticated = true
+    },
+    removeUser: (state) => {
+      state.isAuthenticated = false
     }
-}
+  }
 })
 
- export default userSlice.reducer;
- export const {addUser, removeUser} = userSlice.actions
+export default userSlice.reducer;
+export const { addUser, removeUser } = userSlice.actions
+
 
